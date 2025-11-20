adaccbe0f2e852b1028cc3eba0f753891154281e
Björn Wilmsmann <bjoern@bjoernkw.com>
2025-11-21 00:51:38 +0100

    Initialised with Spring Boot 4

---
+++ src/main/java/de/workshops/ticketeer/TicketeerApplication.java
@@ -0,0 +1,13 @@
+package de.workshops.ticketeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/workshops/ticketeer/TicketeerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.workshops.ticketeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
