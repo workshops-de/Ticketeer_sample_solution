7512612f9c61c66b5cbf4053b9c4014331f73eb7
Birgit Kratz <mail@birgitkratz.de>
2025-12-12 14:43:10 +0100

    Sample solution for exercise 1.0

---
+++ src/main/java/de/workshops/ticketeer/TicketeerStartController.java
@@ -0,0 +1,16 @@
+package de.workshops.ticketeer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ping")
+public class TicketeerStartController {
+
+    @GetMapping
+    public String ping() {
+        return """
+                {"status": "ticketeer-ok"}""";
+    }
+}
