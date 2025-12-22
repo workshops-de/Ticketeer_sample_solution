952e16f2803ebf02d4338df2771253d25e4e8bcf
Birgit Kratz <mail@birgitkratz.de>
2025-12-22 14:40:33 +0100

    Sample solution for Exercise 3.0 – Reservation & Order Domain modeling

--- src/main/java/de/workshops/ticketeer/NotificationException.java
+++ src/main/java/de/workshops/ticketeer/NotificationException.java
@@ -1,7 +1,6 @@
 package de.workshops.ticketeer;
 
 public class NotificationException extends RuntimeException {
-
     public NotificationException(String message) {
         super(message);
     }
