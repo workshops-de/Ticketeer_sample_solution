134635065fb824a21b085fbd7cfb1590488b186e
Birgit Kratz <mail@birgitkratz.de>
2026-01-07 22:10:05 +0100

    Fix for Test `ReservationWithExternalTicketVendorTest`

--- src/main/java/de/workshops/ticketeer/ticketvendor/TicketVendorClientConfiguration.java
+++ src/main/java/de/workshops/ticketeer/ticketvendor/TicketVendorClientConfiguration.java
@@ -1,36 +1,11 @@
 package de.workshops.ticketeer.ticketvendor;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.support.RestClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import org.springframework.web.service.registry.ImportHttpServices;
 
-@Configuration
+
+// This is the new declarative way to instantiate a RestClient
+@Configuration(proxyBeanMethods = false)
+@ImportHttpServices(TicketVendorService.class)
 public class TicketVendorClientConfiguration {
-
-  private final String ticketVendorClientUrl;
-
-  TicketVendorClientConfiguration(
-      @Value("${ticket-vendor.client.url}") String ticketVendorClientUrl
-  ) {
-    this.ticketVendorClientUrl = ticketVendorClientUrl;
-  }
-
-  @Bean
-  HttpServiceProxyFactory httpServiceProxyFactory() {
-    RestClient restClient = RestClient.builder()
-        .baseUrl(ticketVendorClientUrl)
-        .build();
-
-    return HttpServiceProxyFactory.builder()
-        .exchangeAdapter(RestClientAdapter.create(restClient))
-        .build();
-  }
-
-  @Bean
-  TicketVendorService httpBinClient(HttpServiceProxyFactory httpServiceProxyFactory) {
-    return httpServiceProxyFactory.createClient(TicketVendorService.class);
-  }
 }
--- src/test/java/de/workshops/ticketeer/reservation/ReservationWithExternalTicketVendorTest.java
+++ src/test/java/de/workshops/ticketeer/reservation/ReservationWithExternalTicketVendorTest.java
@@ -1,43 +1,34 @@
 package de.workshops.ticketeer.reservation;
 
 import de.workshops.ticketeer.ticketvendor.TicketReservationRequest;
-import de.workshops.ticketeer.ticketvendor.TicketVendorClientConfiguration;
 import de.workshops.ticketeer.ticketvendor.TicketVendorService;
-import java.util.UUID;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.restclient.RestClientCustomizer;
 import org.springframework.boot.restclient.test.autoconfigure.RestClientTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.client.response.MockRestResponseCreators;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
-@RestClientTest(TicketVendorService.class)
-@Import(TicketVendorClientConfiguration.class)
+import java.util.UUID;
+
+@RestClientTest
 class ReservationWithExternalTicketVendorTest {
 
   @Autowired
   private TicketVendorService ticketVendorService;
 
-  @Value("${ticket-vendor.client.url}")
+  @Value("${spring.http.serviceclient.default.base-url}")
   String ticketVendorClientUrl;
 
+  @Autowired
   private MockRestServiceServer mockRestServiceServer;
-
-  @BeforeEach
-  void setUp() {
-    // TODO: MockRestServiceServer doesn't seem to with RestClient, see https://github.com/spring-projects/spring-boot/issues/38832
-    mockRestServiceServer = MockRestServiceServer
-        .bindTo(
-            RestClient
-                .builder()
-                .baseUrl(ticketVendorClientUrl)
-        )
-        .build();
-  }
 
   @Test
   void testReservationWithExternalTicketVendor() {
@@ -66,5 +57,30 @@ class ReservationWithExternalTicketVendorTest {
 
     // Assert
     assert (response.getStatusCode().is2xxSuccessful());
+  }
+
+  // For tests annotated with @RestClientTest, we still need an imperative way to define the RestClient
+  @TestConfiguration
+  static class TestConfig {
+    @Value("${spring.http.serviceclient.default.base-url}")
+    String baseUrl;
+
+    @Bean
+    public RestClient restClient(RestClient.Builder builder) {
+      return builder.build();
+    }
+
+    @Bean
+    public RestClientCustomizer restClientCustomizer() {
+      return (restClientBuilder) -> restClientBuilder
+          .baseUrl(baseUrl);
+    }
+
+    @Bean
+    TicketVendorService ticketVendorService(RestClient restClient) {
+      var adapter = RestClientAdapter.create(restClient);
+      var factory = HttpServiceProxyFactory.builderFor(adapter).build();
+      return factory.createClient(TicketVendorService.class);
+    }
   }
 }
